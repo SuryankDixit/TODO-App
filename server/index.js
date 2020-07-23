bcbd5d77ff29c0fc19b40f63a885fe7f2be452f9
SuryankDixit <suryankdixit@gmail.com>
2020-07-23 10:58:04 +0530

    Created Basic structure

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const db = require('./db');
+
+const app = express();
+
+
+app.get("/", function(req, res) {
+    res.send("Hello World!!!");
+})
+
+app.listen(10000, function() {
+    console.log("Server has started");
+})
